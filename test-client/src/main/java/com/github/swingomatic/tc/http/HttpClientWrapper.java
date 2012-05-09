98296146ccc3a72047b9dc6f6ec666fd715f0dd3
williamsde <williamsde@gmail.com>
2012-05-09 17:04:08 -0400

    Added base functionality for HttpClient

---
+++ test-client/src/main/java/com/github/swingomatic/tc/http/HttpClientWrapper.java
@@ -0,0 +1,91 @@
+package com.github.swingomatic.tc.http;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpException;
+import org.apache.http.HttpStatus;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.ByteArrayBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+public class HttpClientWrapper {
+
+
+	/** The Constant ENCONDING. */
+	private static final String ENCONDING = HTTP.DEFAULT_CONTENT_CHARSET;
+
+	/** The Constant logger. */
+	private static final Logger LOGGER = Logger
+			.getLogger(HttpClientWrapper.class.getName());
+	
+	/**
+	 * Post
+	 *
+	 * @param adiXML the adi xml
+	 * @param adiUserName the user name
+	 * @param adiPassword the password
+	 * @param adiURL the adi url
+	 * @param requestType the request type
+	 * @throws ADIRequestException the aDI service request exception
+	 */
+	public String post(final String url, final String data)
+			throws Exception {
+	
+		HttpClient client = null;
+		String response = null;
+		
+		try {
+			
+			LOGGER.log(Level.INFO,"Posting data to: " +url);
+			
+			client = new DefaultHttpClient();
+			//Setup the request headers
+			client.getParams().setParameter(
+					CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+	
+			HttpPost post = new HttpPost(url);
+
+			MultipartEntity entity = new MultipartEntity(
+			 HttpMultipartMode.BROWSER_COMPATIBLE );
+
+			// For File parameters
+			if(StringUtils.isNotEmpty(data)){
+				ByteArrayBody bab = new ByteArrayBody(data.getBytes(),"");
+				entity.addPart( "", bab);
+				post.setEntity(entity);
+			}
+
+			// Here we go!
+			response = EntityUtils.toString(client.execute(post)
+					.getEntity(), ENCONDING);
+			
+			return response;
+			
+		} catch (Exception e) {
+			LOGGER.log(Level.SEVERE,"Error occured posting request",e);
+			throw e;
+		} finally {
+			try {
+				client.getConnectionManager().shutdown();
+			} catch (Exception e) {
+				LOGGER.log(Level.SEVERE,"Error occured shutting down HTTP client",e);
+				/* No Op */
+			}
+		}
+
+	}
+	
+}
---
+++ test-client/src/test/java/co/github/swingomatic/tc/test/HttpClientWrapperTest.java
@@ -0,0 +1,77 @@
+package co.github.swingomatic.tc.test;
+
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import static org.junit.Assert.*;
+import com.github.swingomatic.tc.http.HttpClientWrapper;
+
+
+/**
+ * The Class HttpClientWrapperTest.
+ */
+public class HttpClientWrapperTest {
+
+	/** The Constant LIST_COMPONENTS_URL. */
+	private final static String LIST_COMPONENTS_URL = "http://localhost:8088/list-components";
+
+	/** The http client. */
+	private HttpClientWrapper httpClient;
+	
+	/**
+	 * Test post.
+	 */
+	@Test
+	public void testPost() throws Exception{
+		httpClient = new HttpClientWrapper();		
+		String response = httpClient.post(LIST_COMPONENTS_URL, null);
+		assertNotNull(getValue("/list",response));
+	}
+	
+	
+	
+	
+	//helper methods
+	
+	/**
+	 * Gets the scale value from the supplied xml.
+	 *
+	 * @param xpathExpression the xpath expression
+	 * @param xml the xml
+	 * @return the value
+	 * @throws Exception the exception
+	 */
+	private String getValue(String xpathExpression, String xml) throws Exception {
+
+		String value = null;
+
+		// Standard of reading a XML file
+		Document xmlDocument = DocumentBuilderFactory.newInstance()
+				.newDocumentBuilder()
+				.parse(new InputSource(new StringReader(xml)));
+		XPathExpression expr = null;
+
+		// Create a XPathFactory
+		XPathFactory xFactory = XPathFactory.newInstance();
+
+		// Create a XPath object
+		XPath xpath = xFactory.newXPath();
+
+		// Compile the XPath expression
+		expr = xpath.compile(xpathExpression);
+		// Run the query and get a nodeset
+		value = expr.evaluate(xmlDocument);
+
+		return value;
+
+	}
+	
+}
