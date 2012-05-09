0e0dd879fe57720089c7f657f190de9243796891
moscac <moscac@new-host-3.home>
2012-05-09 12:48:10 -0400

    initial commit

---
+++ server/src/main/java/com/github/swingomatic/message/ComponentInfo.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.swingomatic.message;
+
+/**
+ *
+ * @author Carl J. Mosca
+ */
+public class ComponentInfo {
+    private String name;
+    private String clazz;
+    private String ofLabel;
+    private String text;
+    
+    public ComponentInfo(String name, String clazz) {
+        this.name = name;
+        this.clazz = clazz;
+    }
+
+    public ComponentInfo(String name, String clazz, String ofLabel, String text) {
+        this.name = name;
+        this.clazz = clazz;
+        this.ofLabel = ofLabel;
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClazz() {
+        return clazz;
+    }
+
+    public void setClazz(String clazz) {
+        this.clazz = clazz;
+    }
+
+    public String getOfLabel() {
+        return ofLabel;
+    }
+
+    public void setOfLabel(String ofLabel) {
+        this.ofLabel = ofLabel;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+}
