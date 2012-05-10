5dcd493ba27b57174d06f83c0bad13019ca9e9c9
Carl J. Mosca <carljmosca@gmail.com>
2012-05-10 17:39:39 -0400

    complete XML support

--- test-client/src/test/java/co/github/swingomatic/tc/test/HttpClientWrapperTest.java
+++ test-client/src/test/java/co/github/swingomatic/tc/test/HttpClientWrapperTest.java
@@ -13,64 +13,76 @@ import org.junit.Test;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 
-
 /**
  * The Class HttpClientWrapperTest.
  */
 public class HttpClientWrapperTest {
 
-	/** The Constant LIST_COMPONENTS_URL. */
-	private final static String LIST_COMPONENTS_URL = "http://localhost:8088/list-components";
+    /**
+     * The Constant LIST_COMPONENTS_URL.
+     */
+    private final static String LIST_COMPONENTS_URL = "http://localhost:8088/list-components";
+    private final static String MAIN_URL = "http://localhost:8088";
+    /**
+     * The http client.
+     */
+    private HttpClientWrapper httpClient;
 
-	/** The http client. */
-	private HttpClientWrapper httpClient;
-	
-	/**
-	 * Test post.
-	 */
-	@Test
-	public void testPost() throws Exception{
-		httpClient = new HttpClientWrapper();		
-		String response = httpClient.post(LIST_COMPONENTS_URL, null);
-                XStream xstream = new XStream();
-                ApplicationCommand ac = (ApplicationCommand)xstream.fromXML(response);
-                assertNotNull(ac);
-		assertNotNull(getValue("/list",response));
-	}
-	
-	
-	
-	
-	//helper methods
-	
-	/**
-	 * Gets the scale value from the supplied xml.
-	 *
-	 * @param xpathExpression the xpath expression
-	 * @param xml the xml
-	 * @return the value
-	 * @throws Exception the exception
-	 */
-	private String getValue(String xpathExpression, String xml) throws Exception {
+    /**
+     * Test post.
+     */
+//    @Test
+//    public void testPost() throws Exception {
+//        httpClient = new HttpClientWrapper();
+//        String response = httpClient.post(LIST_COMPONENTS_URL, null);
+//        XStream xstream = new XStream();
+//        ApplicationCommand ac = (ApplicationCommand) xstream.fromXML(response);
+//        assertNotNull(ac);
+//        assertNotNull(getValue("/list", response));
+//    }
 
-		String value = null;
-		
-		// Standard of reading a XML file
-		Document xmlDocument = DocumentBuilderFactory.newInstance()
-				.newDocumentBuilder()
-				.parse(new InputSource(new StringReader(xml)));
-		XPathExpression expr = null;
-		// Create a XPathFactory
-		XPathFactory xFactory = XPathFactory.newInstance();
-		// Create a XPath object
-		XPath xpath = xFactory.newXPath();
-		// Compile the XPath expression
-		expr = xpath.compile(xpathExpression);
-		// Run the query and get a nodeset
-		value = expr.evaluate(xmlDocument);
+    @Test
+    public void testApplicationCommand() throws Exception {
+        httpClient = new HttpClientWrapper();
+        XStream xstream = new XStream();
+        ApplicationCommand ac = new ApplicationCommand();
+        ac.setCommand("list-components");
+        String message = xstream.toXML(ac);
+        String response = httpClient.doCall(MAIN_URL, "/", message); //, (message));
+        int p = response.indexOf("?>");
+        if (p >= 0) {
+            response = response.substring(p + 2);
+        }
+        ac = (ApplicationCommand) xstream.fromXML(response);
+        assertNotNull(ac);
+    }
 
-		return value;
+    //helper methods
+    /**
+     * Gets the scale value from the supplied xml.
+     *
+     * @param xpathExpression the xpath expression
+     * @param xml the xml
+     * @return the value
+     * @throws Exception the exception
+     */
+    private String getValue(String xpathExpression, String xml) throws Exception {
 
-	}
-	
+        String value = null;
+
+        // Standard of reading a XML file
+        Document xmlDocument = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(xml)));
+        XPathExpression expr = null;
+        // Create a XPathFactory
+        XPathFactory xFactory = XPathFactory.newInstance();
+        // Create a XPath object
+        XPath xpath = xFactory.newXPath();
+        // Compile the XPath expression
+        expr = xpath.compile(xpathExpression);
+        // Run the query and get a nodeset
+        value = expr.evaluate(xmlDocument);
+
+        return value;
+
+    }
 }
