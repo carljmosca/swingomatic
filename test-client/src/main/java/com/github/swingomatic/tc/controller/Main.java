fde93db58747f219f5806d466ecf19f15c196757
Carl J. Mosca <Carl J. Mosca@10.0.2.15>
2012-05-14 07:48:21 -0400

    created new package

---
+++ test-client/src/main/java/com/github/swingomatic/tc/controller/Main.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.swingomatic.tc.controller;
+
+/**
+ *
+ * @author Carl J. Mosca
+ */
+public class Main {
+    
+}
