22892fcf3a6ad279f011add73b2f632e751f234c
Carl J. Mosca <Carl J. Mosca@10.0.2.15>
2012-05-15 11:28:55 -0400

    updated JavaFX

--- test-client/src/main/java/com/github/swingomatic/tc/Main.java
+++ test-client/src/main/java/com/github/swingomatic/tc/Main.java
@@ -7,10 +7,12 @@ package com.github.swingomatic.tc;
 import com.github.swingomatic.message.ApplicationCommand;
 import com.github.swingomatic.message.ComponentInfo;
 import java.util.List;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import org.apache.log4j.Logger;
 
 /**
@@ -24,28 +26,39 @@ public class Main {
 
     @FXML
     private void btnListAction(ActionEvent event) {
+        load();
     }
     
     @FXML
+    private TextField txtServerAddress;
+    
+    @FXML
     private TableView tblList;
-
     private static Logger logger = Logger.getLogger(Main.class);
 
     public void load() {
-        Client client = new Client();
-        ApplicationCommand ac = new ApplicationCommand();
-        ac.setCommand("list-components");
-        try {
-            ac = client.execute("http://localhost:8088", ac);
-            List<ComponentInfo> list = ac.getComponents();
-            for (ComponentInfo componentInfo : list) {
-                System.out.println(componentInfo.toString());
+        Platform.runLater(new Runnable() {
+
+            @Override
+            public void run() {
+                String host = "http://localhost:8088";
+                if (txtServerAddress.getText().length() > 0) {
+                    host = txtServerAddress.getText();
+                }
+                Client client = new Client();
+                ApplicationCommand ac = new ApplicationCommand();
+                ac.setCommand("list-components");
+                try {
+                    ac = client.execute(host, ac);
+                    List<ComponentInfo> list = ac.getComponents();
+                    tblList.getItems().clear();
+                    tblList.getItems().addAll(list);
+                } catch (Exception ex) {
+                    logger.error(ex.getMessage());
+                }
             }
-            tblList.getItems().clear();
-            tblList.getItems().addAll(list);
-        } catch (Exception ex) {
-            logger.error(ex.getMessage());
-        }
+        });
+
 
     }
 }
