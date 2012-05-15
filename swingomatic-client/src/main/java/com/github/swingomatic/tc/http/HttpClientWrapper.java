84db2ea204d3a0b7d1f98bbaa18da6a18b72610f
moscac <moscac@new-host-3.home>
2012-05-14 20:03:56 -0400

    removed unused imports/libraries

--- swingomatic-client/src/main/java/com/github/swingomatic/tc/http/HttpClientWrapper.java
+++ swingomatic-client/src/main/java/com/github/swingomatic/tc/http/HttpClientWrapper.java
@@ -6,26 +6,10 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.net.ssl.SSLSocketFactory;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpVersion;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.ByteArrayBody;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.CoreProtocolPNames;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
 
 public class HttpClientWrapper {
 
